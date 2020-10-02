4d2a50b82b1cb95462aff4f944ba93584870461b
Melhor <b05705046@ntu.edu.tw>
2020-10-02 15:48:06 +0800

    finish navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
