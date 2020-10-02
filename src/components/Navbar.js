762348d17cec6021b2032be4d0de4b96f8dd03f0
Melhor <b05705046@ntu.edu.tw>
2020-10-02 19:28:39 +0800

    fix responsive toggle issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ function Navbar() {
                 Wendee Hsu
                 <img className="navbar-icon" src="/icon.png" />
             </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div className="collapse navbar-collapse" id="menu">
                 <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                     <li className="nav-item">
                         <a className="nav-link" href="#">Work</a>
