c8c702cf0239e52d8019dd29af18aed3798eaf82
Melhor <b05705046@ntu.edu.tw>
2020-10-03 20:47:56 +0800

    finish My Work Page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function ProjectCard() {
-    const img = "/images/project1-cover.png";
-    return (
-        <div className="row justify-content-center projectCard">
-            <div className="col-3 projectDescription">
-                <h5>Website Redesign</h5>
-                <h6 className="gray-text">UX Researcher</h6>
-                <h6 className="pink-text">PDIS</h6>
+class ProjectCard extends Component {
+    render(){
+        return (
+            <div className="row justify-content-center projectCard">
+                <div className="col-3 projectDescription">
+                    <h5>{this.props.project.name}</h5>
+                    <h6 className="gray-text">{this.props.project.role}</h6>
+                    <h6 className="pink-text">{this.props.project.company}</h6>
+                </div>
+                <div className="cardBorder">
+                    <div 
+                        className="card" 
+                        style={{backgroundImage: "url(" + process.env.PUBLIC_URL + this.props.project.imgUrl + ")"}}
+                    />
+                </div>
             </div>
-            <div className="cardBorder">
-                <div 
-                    className="card" 
-                    style={{backgroundImage: "url(" + process.env.PUBLIC_URL + img + ")"}}
-                />
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default ProjectCard;
--- src/components/Work.js
+++ src/components/Work.js
@@ -4,16 +4,41 @@ import '../css/Work.css';
 
 class Work extends Component {
     render() {
+        const project1 = new Project(
+            "Redesign Website of Bureau of Labor Insurance",
+            "UX Researcher",
+            "PDIS",
+            "/images/project1-cover.png",
+            ""
+        );
+
+        const project2 = new Project(
+            "AR Contact Lens Wearing App",
+            "Frontend Engineer",
+            "Self-Project",
+            "/images/project2-cover.png",
+            ""
+        );
+
         return (
             <div>
                 <h3 className="title">
                     My Work
                 </h3>
-                <ProjectCard />
-                <ProjectCard />
+                <ProjectCard project={project1}/>
+                <ProjectCard project={project2}/>
             </div>
         )
     }
-}
+};
+class Project {
+    constructor(name, role, company, imgUrl, pageUrl) {
+      this.name = name;
+      this.role = role;
+      this.company = company;
+      this.imgUrl = imgUrl;
+      this.pageUrl = pageUrl;
+    }
+};
 
 export default Work;
