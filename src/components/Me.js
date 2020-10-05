d6b0adccb3b353442ac863eea1887661d3e491a4
Melhor <b05705046@ntu.edu.tw>
2020-10-05 19:36:35 +0800

    finish Me page

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faFileInvoice } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import ReactTooltip from 'react-tooltip';
+import "../css/Me.css";
 
 export default class Me extends Component {
     render() {
@@ -9,11 +14,27 @@ export default class Me extends Component {
                 </h3>
                 <div className="row justify-content-center">
                     <h6 className="gray-text col-6 center-text">
-                        Frontend developer, mechine learning engineer<br/>
+                        Frontend Developer, Mechine Learning Engineer<br/>
                         I study information management in National Taiwan University.
                         Now exploring the field of user-centered design and seeking for new challenges.
                     </h6>
                 </div>
+                <br/>
+                <div className="row justify-content-center">
+                    <a data-tip="LinkedIn" href="https://www.linkedin.com/in/%E9%81%A0%E5%BF%97-%E5%BE%90-092bab177/" target="_blank">
+                        <FontAwesomeIcon className="icon" icon={faLinkedin} />
+                    </a>
+                    <a data-tip="Github" href="https://github.com/wendeehsu" target="_blank">
+                        <FontAwesomeIcon className="icon" icon={faGithub} />
+                    </a>
+                    <a data-tip="Email" href="mailto:wendeehsu@gmail.com" target="_blank">
+                        <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                    </a>
+                    <a data-tip="Resume" href="https://drive.google.com/file/d/1II5D7NVim0pw6dulTW-NZpHEzmoGR4gJ/view?usp=sharing" target="_blank">
+                        <FontAwesomeIcon className="icon" icon={faFileInvoice} />
+                    </a>
+                </div>
+                <ReactTooltip effect="solid" place="bottom" />
             </div>
         )
     }
