98a4cb33ed8d586fcc784373c7a8cbb5550d8f5e
Melhor <b05705046@ntu.edu.tw>
2020-10-22 09:29:44 +0800

    finish design process

--- src/components/Projects/RedesignBLI.js
+++ src/components/Projects/RedesignBLI.js
@@ -34,35 +34,38 @@ class RedesignBLI extends Component {
                 </div>
                 <div className="section">
                     <h6>PROCESS OVERVIEW</h6>
-                    <img className="full-width-img" src="/images/Process1.png" />
+                    <img className="full-width-img" src="/images/BLI/Process.png" />
                 </div>
                 <div className="section">
                     <h6>DESIGN PROCESS</h6>
+
+                    {/* ---- 1. User Reasearch ---- */}
                     <p className="secondary-title">1. User Reasearch</p>
                     <p className="section-subtitle">Interview</p>
                     <div className="row">
                         <p className="col-6">
                             To find current users of the e-service platform of the Bureau of Labor Insurance, we released questionnaires asking about people’s experience of using the website. We later selected 5 users and conducted semi-structured interviews with them. To include thoughts from our stakeholders into the new design, we also arranged meetings with 4 staff working in the Information Technology Division of the Bureau of Labor Insurance.
                         </p>
-                        <img className="full-width-img col-6" src="/images/Test.png" />
+                        <img className="full-width-img col-6" src="/images/BLI/Test.png" />
                     </div>
                     <p className="section-subtitle">Affinity Diagram</p>
                     <div className="row">
                         <p className="col-6">
                             After interviewing each user and stakeholders, we sorted more than 100 user quotes, derived 25 insights, and generated 3 personas.
                         </p>
-                        <img className="full-width-img col-6" src="/images/AffinityDiagram.png" />
+                        <img className="full-width-img col-6" src="/images/BLI/AffinityDiagram.png" />
                     </div>
                     <p className="section-subtitle">User Types</p>
                     <p>
                         Based on whether these users are familiar with their goal on the website and their degree of attention they pay to their right of labor insurance, we made 3 personas corresponding to each group we want to target.
                     </p>
-                    <img className="full-width-img" src="/images/UserVector.png" />
+                    <img className="full-width-img" src="/images/BLI/UserVector.png" />
                     <p>
                         For each group, we identified their needs and pain points:
                     </p>
-                    <img className="full-width-img" src="/images/UserTable.png" />
+                    <img className="full-width-img" src="/images/BLI/UserTable.png" />
 
+                    {/* ---- 2. Define Problem ---- */}
                     <p className="secondary-title">2. Define Problem</p>
                     <div className="div-box">
                         <p className="vertical-center">
@@ -75,7 +78,42 @@ class RedesignBLI extends Component {
                     </p>
                     <p className="section-subtitle">Limitation</p>
                     <p>
-                        Bureau of Labor Insurance supports a wide variety of services for people facing different incidences, but the website has a barely low rate of usage. A person may visit the website no more than twice a year. As a result, we should regard each person as first-time users when designing guidance.                    </p>
+                        Bureau of Labor Insurance supports a wide variety of services for people facing different incidences, but the website has a barely low rate of usage. A person may visit the website no more than twice a year. As a result, we should regard each person as first-time users when designing guidance.                    
+                    </p>
+
+                    {/* ---- 3. Design Wireframe ---- */}
+                    <p className="secondary-title">3. Design Wireframe</p>
+                    <p className="section-subtitle">Using different information architecture to design two versions of wireframe</p>
+                    <p>
+                        Since the Bureau of Labor Insurance provides a wide variety of services, we made two versions of wireframe using different information architecture to categorize each service. The “Stage of Life” version divides services into “working, retired, disabled, unemployed, giving birth, dead”. As for the “Function” version, we simply labeled each service into “making inquiries, applying, paying, changing data, printing”.
+                    </p>
+                    <img className="full-width-img" src="/images/BLI/IaCompare.png" />
+                    <p className="section-subtitle">Usability Testing on Information Architecture</p>
+                    <p>
+                        We conducted 5 usability testing, carefully selected users with diverse backgrounds. Besides testing our current user flow, we were more interested in which information architecture do they prefer.
+                    </p>
+                    <div className="row">
+                        <p className="col-6">
+                            Surprisingly, users found it difficult to search for the service they want in the “Stage of Life” version. For example, there is a subsidy that can only be applied when one was unemployed but later got a new job in 3 months. In this case, some may categorize it as “unemployed”, while others think it should belong to “working”. Due to the lower degree of confusion raised in classifying each service, we decided to use the “Function” version in our future iteration.                        </p>
+                        <img className="full-width-img col-6" src="/images/BLI/Wireframe.png" />
+                    </div>
+
+                    {/* ---- 4. Creating Propotype ---- */}
+                    <p className="secondary-title">4. Creating Propotype</p>
+                    <p className="section-subtitle">Login</p>
+                    <img className="flow-img" src="/images/BLI/Prototype_Login.png"/>
+                    <p className="section-subtitle">Applying for a service</p>
+                    <img className="full-width-img" src="/images/BLI/Prototype_Apply.png"/>
+
+                    {/* ---- 5. Usability Testing ---- */}
+                    <p className="secondary-title">5. Usability Testing</p>
+                    <p>
+                        After designing our low-fi prototype, we arranged 3 one-to-one usability testing to see how the current design can be improved. We later iterated the low-fi prototype into our high-fi prototype, conducting 8 usability testing with both first-time users and experienced users of the website.
+                    </p>
+                    <div className="row justify-content-center">
+                        <img className="center-img-space" src="/images/BLI/TestingFlow.png"/>
+                    </div>
+
                 </div>
             </div>
         )
