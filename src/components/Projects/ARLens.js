8539dd599d75fd94b8111a627e5a8e2f4d1eee1b
Melhor <b05705046@ntu.edu.tw>
2020-10-27 20:31:38 +0800

    finish AR

--- src/components/Projects/ARLens.js
+++ src/components/Projects/ARLens.js
@@ -50,20 +50,24 @@ class ARLens extends Component {
                         We should seek an offline approach on iPad 6 to imitate the real experience of trying on a color contact lens.
                     </p>
                     <div className="row justify-content-center">
-                        <img className="restriction-icon" src="/images/AR/Restriction1.png" />
-                        <img className="restriction-icon" src="/images/AR/Restriction2.png" />
-                        <img className="restriction-icon" src="/images/AR/Restriction3.png" />
-                    </div>
-                    <div className="row justify-content-center">
-                        <p className="col text-center gray-text">
-                            The app will be installed on iPad 6, which does not have a true depth camera that natively captures face data.
-                        </p>
-                        <p className="col text-center gray-text">
-                            Apple’s ARkit cannot produce a contact lens wearing experience that is true enough.
-                        </p>
-                        <p className="col text-center gray-text">
-                            The contact lens wearing service needs to be executable offline. 
-                        </p>
+                        <div className="col">
+                            <img className="restriction-icon" src="/images/AR/Restriction1.png" />
+                            <p className="col text-center gray-text">
+                                The app will be installed on iPad 6, which does not have a true depth camera that natively captures face data.
+                            </p>
+                        </div>
+                        <div className="col">
+                            <img className="restriction-icon" src="/images/AR/Restriction2.png" />
+                            <p className="col text-center gray-text">
+                                Apple’s ARkit cannot produce a contact lens wearing experience that is true enough.
+                            </p>
+                        </div>
+                        <div className="col">
+                            <img className="restriction-icon" src="/images/AR/Restriction3.png" />
+                            <p className="col text-center gray-text">
+                                The contact lens wearing service needs to be executable offline. 
+                            </p>
+                        </div>
                     </div>
                     
                     <p className="section-subtitle pink-text">Solution</p>
@@ -76,8 +80,68 @@ class ARLens extends Component {
                     <p className="gray-text">
                         The len-wearing algorithm was developed on a Windows 10 laptop by my teammate Jenny Ho using C++, while I was in charge of reproducing it on iPad 6.
                     </p>
+
+                    {/* ---- 2. Develop iOS App ---- */}
+                    <p className="secondary-title">2. Develop iOS App</p>
+                    <p>
+                        After we finished the main algorithm, Formosa Optical, the owner of this project, sent us the user interface design for each page. When developing the iOS app, we encountered several technical problems as listed below.
+                    </p>
+                    <img className="full-width-img" src="/images/AR/DevelopTable.png" />
                 </div>
 
+                <div className="section">
+                    <h5>FINAL PRODUCT</h5>
+                    <p className="section-subtitle pink-text">Scenario</p>
+                    <p>
+                        At the store, the staff will login with his/her account. They will later let customers try the contact lens wearing service.
+                    </p>
+                </div>
+                <div className="full-width-section gray-bg">
+                    <img className="full-width-img" src="/images/AR/FinalProduct.png" />
+                </div>
+
+                <div className="section">
+                    <h5>RESULT</h5>
+                    <p>
+                        Our app is able to render a color contact lens on users’ iris in almost real-time. Customers can try on more than <b>10 lenses</b> in one minute.
+                    </p>
+                </div>
+
+                <div className="section">
+                    <h5>REFLECTION</h5>
+                    <div className="row">
+                        <img className="reflection-icon col" src="/images/AR/Reflection1.png" />
+                        <div className="col">
+                            <p className="bold-text">
+                                1. Doing a side project is the fastest way to pick up a new skill                            
+                            </p>
+                            <p>
+                                This is my first time to develop a pure iOS app. In the past, I used to learn step by step from following tutorials on Codecademy to finishing little applications later. However, since the project has a strict deadline this time, I had no choice but to jump into development right away. Checking what feature needs to be done next, I searched for resources online that taught me how to implement it. I certainly encountered a lot of problems since I didn’t even know the basics in Swift and Objective C. Nevertheless, I quickly build up the knowledge in the process of troubleshooting.                            
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <img className="reflection-icon col" src="/images/AR/Reflection2.png" />
+                        <div className="col">
+                            <p className="bold-text">
+                                2. Don’t be afraid to show “I need help”                            </p>
+                            <p>
+                                After my team member finished the computer vision algorithm using C++, I was in charge of executing it on iOS along with all the rest of the frontend development. During that time, I had little knowledge of both computer vision and iOS development. As a result, I was aimless when the app was not working. It took me months struggling on my own but in vain. I later decided to ask my team members for help (and also posted questions on the developer’s forum.) Things became much easier when others provided their previous experience. With their assistance, I not only learned how computer vision worked but was also able to solve similar questions posted by other developers on the forum.
+                            </p>
+                            </div>
+                    </div>
+                    <div className="row">
+                        <img className="reflection-icon col" src="/images/AR/Reflection3.png" />
+                        <div className="col">
+                            <p className="bold-text">
+                                3. We should only start a project when the contract is completed
+                            </p>
+                            <p>
+                            Having no experience in working for people outside the school, our team jumped into development before any contract was done. We later realized the owner kept demanding new functions and requirements during our cooperation. The suffering of project creeping was real, using up extra time and effort but not receiving any additional payments. From this experience, we found out how important a contract was as it will serve as protection under this situation.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
