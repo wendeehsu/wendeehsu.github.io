68888a02292a1d6da0f1fc6fef89283621ee8afb
wendee <b05705046@ntu.edu.tw>
2022-02-04 10:57:08 +0800

    fix: remove horizontal scroll in work.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,8 +5,8 @@ class ProjectCard extends Component {
     render(){
         return (
             <Link to={this.props.project.pageUrl}>
-                <div className="row justify-content-center projectCard">
-                    <div className="col-3 projectDescription">
+                <div className="justify-content-center projectCard">
+                    <div className="projectDescription">
                         <h5>{this.props.project.name}</h5>
                         <h6 className="gray-text">{this.props.project.role}</h6>
                         <h6 className="pink-text">{this.props.project.company}</h6>
