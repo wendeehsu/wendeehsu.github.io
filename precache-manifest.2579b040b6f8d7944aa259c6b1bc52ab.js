e41e7bd1bf70f91227d2b3bc703e2cc800f5e790
wendeehsu <b05705046@ntu.edu.tw>
2023-09-14 17:11:35 -0500

    Updates

--- precache-manifest.2579b040b6f8d7944aa259c6b1bc52ab.js
+++ precache-manifest.2579b040b6f8d7944aa259c6b1bc52ab.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "268975d4d06d03c8baad2fe2f6bee62b",
+    "revision": "833da1132eed63cfd958b156fdb2c334",
     "url": "./index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.1cace5d8.chunk.css"
   },
   {
-    "revision": "faf8ce49f0e9a77280d6",
-    "url": "./static/css/main.4a0b8f92.chunk.css"
+    "revision": "328bf2b59091663fba1b",
+    "url": "./static/css/main.dd4ac07b.chunk.css"
   },
   {
     "revision": "ea4786969e1cd8927747",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.582a3db8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "faf8ce49f0e9a77280d6",
-    "url": "./static/js/main.1fd52057.chunk.js"
+    "revision": "328bf2b59091663fba1b",
+    "url": "./static/js/main.8a455eac.chunk.js"
   },
   {
     "revision": "610a38b7b5a7cb7b2d5e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1a538ba21585014b7c5fd8a12ba11eb4.js"
+  "./precache-manifest.2579b040b6f8d7944aa259c6b1bc52ab.js"
 );
 
 self.addEventListener('message', (event) => {
