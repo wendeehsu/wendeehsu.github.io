b6beaf1ad9cd67383e38edb5e631e59e9a72510a
wendeehsu <b05705046@ntu.edu.tw>
2024-01-07 19:31:20 -0600

    fix: BLI responsive

--- src/container/Projects/RedesignBLI.js
+++ src/container/Projects/RedesignBLI.js
@@ -43,7 +43,7 @@ export default function RedesignBLI() {
   const reflections = [{
     imgUrl: imageBaseUrl + "Reflection1.png",
     title: "1. Testing scenario may affect our result",
-    detail: "As mentioned above, we figured out users prefer the functional version of categorizing service during wireframe’s usability testing. However, reflecting on this decision, our team think the testing scenario may affect users’ preference. Some users were heavily aware that they were doing a test, as they told us “I finished this question.” instead of “I finished the application service.” Not being able to put themselves in the scenario can result in behaving differently in comparison with real-life scenarios. In the future, besides acknowledging each tool has its limitation, we may also add more descriptions to help testers get into the scenario."
+    detail: "As mentioned above, we figured out users prefer the functional version of categorizing service during wireframe’s usability testing. However, reflecting on this decision, our team think the testing scenario may affect users’ preference. Some users were heavily aware that they were doing a test, as they told us 'I finished this question.' instead of 'I finished the application service.' Not being able to put themselves in the scenario can result in behaving differently in comparison with real-life scenarios. In the future, besides acknowledging each tool has its limitation, we may also add more descriptions to help testers get into the scenario."
   }, {
     imgUrl: imageBaseUrl + "Reflection2.png",
     title: "2. It is important to point out the value of UX",
@@ -82,6 +82,7 @@ export default function RedesignBLI() {
             <a
               href="https://www.linkedin.com/in/ping-ju-huang-aa0b10174/"
               target="_blank"
+              rel="noreferrer"
             >
               Ping-Ju Huang
             </a>{" "}
@@ -91,13 +92,17 @@ export default function RedesignBLI() {
             <a
               href="https://www.linkedin.com/in/jennifer-chueh-chen-l-218533195/"
               target="_blank"
+              rel="noreferrer"
             >
               Chueh-Chen Lai
             </a>{" "}
             - UX Researcher, UI Designer
           </li>
           <li>
-            <a href="https://medium.com/@as964119/about" target="_blank">
+            <a
+              href="https://medium.com/@as964119/about"
+              target="_blank"
+              rel="noreferrer">
               Nuan-Ting Lin
             </a>{" "}
             - UX Researcher, UI Designer
@@ -132,7 +137,6 @@ export default function RedesignBLI() {
       <div className="section">
         <h2>Process Overview</h2>
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}Process.png`}
           alt="research, define, design, test"
         />
@@ -180,13 +184,12 @@ export default function RedesignBLI() {
           target.
         </p>
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}UserVector.png`}
           alt="two vectors of dividing users: familiarity to the system and the amount of attention to their right of labor insurance"
         />
+        <br />
         <p>For each group, we identified their needs and pain points:</p>
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}UserTable.png`}
           alt="different users have different needs"
         />
@@ -201,13 +204,11 @@ export default function RedesignBLI() {
             difficulties in different stages?
           </p>
         </div>
-        <br />
         <h4 className="blue-text">User Need</h4>
         <p>
           Users expect the website to provide a clear guide and immediate
           feedback when searching or applying for a service.
         </p>
-        <br />
         <h4 className="blue-text">Limitation</h4>
         <p>
           Bureau of Labor Insurance supports a wide variety of services for
@@ -229,13 +230,12 @@ export default function RedesignBLI() {
           Since the Bureau of Labor Insurance provides a wide variety of
           services, we made two versions of wireframe using different
           information architecture to categorize each service. The{" "}
-          <b>“Stage of Life”</b> version divides services into “working,
-          retired, disabled, unemployed, giving birth, dead”. As for the{" "}
-          <b>“Function”</b> version, we simply labeled each service into “making
-          inquiries, applying, paying, changing data, printing”.
+          <b>"Stage of Life"</b> version divides services into "working,
+          retired, disabled, unemployed, giving birth, dead". As for the{" "}
+          <b>"Function"</b> version, we simply labeled each service into "making
+          inquiries, applying, paying, changing data, printing".
         </p>
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}IaCompare.png`}
           alt="Comparing Information architecture"
         />
@@ -250,17 +250,17 @@ export default function RedesignBLI() {
         <div className="md:flex flex-wrap">
           <p className="md:flex-1">
             Surprisingly, users found it difficult to search for the service
-            they want in the “Stage of Life” version. For example, there is a
+            they want in the "Stage of Life" version. For example, there is a
             subsidy that can only be applied when one was unemployed but later
             got a new job in 3 months. In this case, some may categorize it as
-            “unemployed”, while others think it should belong to “working”.
+            "unemployed", while others think it should belong to "working".
             <br />
             &#8594; Due to the <b>lower degree of confusion</b> raised in
-            classifying each service, we decided to use the <b>“Function”</b>{" "}
+            classifying each service, we decided to use the <b>"Function"</b>{" "}
             version in our future iteration.
           </p>
           <img
-            className="md:w-[50%]"
+            className="lg:w-[50%]"
             src={`${imageBaseUrl}Wireframe.png`}
             alt="wireframe"
           />
@@ -278,7 +278,6 @@ export default function RedesignBLI() {
         />
         <h4 className="blue-text">Applying for a service</h4>
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}Prototype_Apply.png`}
           alt="Applying page in prototype"
         />
@@ -314,7 +313,7 @@ export default function RedesignBLI() {
 
       <div className="full-width-inner-section gray-bg">
         <Label text="Old" textColor="text-white" bgColor="bg-gray-400" />
-        <img className="full-width-img" src={`${imageBaseUrl}OldLogin.png`} alt="Old Login" />
+        <img src={`${imageBaseUrl}OldLogin.png`} alt="Old Login" />
         <p>
           Users will not know whether their login equipment is successfully
           detected before pressing the login button.
@@ -322,7 +321,7 @@ export default function RedesignBLI() {
       </div>
       <div className="full-width-inner-section">
         <Label text="New" textColor="" bgColor="bg-sky-100" />
-        <img className="full-width-img" src={`${imageBaseUrl}NewLogin.png`} alt="New Login" />
+        <img src={`${imageBaseUrl}NewLogin.png`} alt="New Login" />
         <p>
           Since the Bureau of Labor Insurance is going to support different ways
           to log in, we enable users to choose a suitable option based on the
@@ -340,7 +339,7 @@ export default function RedesignBLI() {
       </div>
       <div className="full-width-inner-section gray-bg">
         <Label text="Old" textColor="text-white" bgColor="bg-gray-400" />
-        <img className="full-width-img" src={`${imageBaseUrl}OldInquiry.png`} alt="old inquiry" />
+        <img src={`${imageBaseUrl}OldInquiry.png`} alt="old inquiry" />
         <p>
           The dropdown list is shown when hovering, which is not friendly for
           the website’s elder users since they are used to clicking buttons.
@@ -350,7 +349,7 @@ export default function RedesignBLI() {
       </div>
       <div className="full-width-inner-section">
         <Label text="New" textColor="" bgColor="bg-sky-100" />
-        <img className="full-width-img" src={`${imageBaseUrl}NewInquiry.png`} alt="new inquiry" />
+        <img src={`${imageBaseUrl}NewInquiry.png`} alt="new inquiry" />
         <p>
           To shorten user’s time spend searching for a service, we use a side
           menu with cards showing each service in the main space.
@@ -365,7 +364,6 @@ export default function RedesignBLI() {
       <div className="full-width-inner-section gray-bg">
         <Label text="Old" textColor="text-white" bgColor="bg-gray-400" />
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}OldQualification.png`}
           alt="Old Qualification"
         />
@@ -379,7 +377,6 @@ export default function RedesignBLI() {
       <div className="full-width-inner-section">
         <Label text="New" textColor="" bgColor="bg-sky-100" />
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}NewQualification.png`}
           alt="New Qualification"
         />
@@ -398,7 +395,6 @@ export default function RedesignBLI() {
       <div className="full-width-inner-section">
         <Label text="New" textColor="" bgColor="bg-sky-100" />
         <img
-          className="full-width-img"
           src={`${imageBaseUrl}NewApplicationProcess.png`}
           alt="New Application Process"
         />
